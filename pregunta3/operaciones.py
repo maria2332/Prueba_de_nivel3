eba33dc1d0245fcb9b7ce3e45cc1d3d1076cbe92
maria2332 <maria.ballesteros2004@gmail.com>
2023-04-21 09:59:50 +0200

    3

--- pregunta3/operaciones.py
+++ pregunta3/operaciones.py
@@ -1,36 +1,36 @@
 def suma(num1, num2):
     try:
         resultado = float(num1) + float(num2)
+        return resultado
     except TypeError:
-        print("Error: Tipo de dato no válido.")
-        resultado = None
-    return resultado
+        print("Error: Tipo de dato no válido")
+        return None
 
 def resta(num1, num2):
     try:
         resultado = float(num1) - float(num2)
+        return resultado
     except TypeError:
-        print("Error: Tipo de dato no válido.")
-        resultado = None
-    return resultado
+        print("Error: Tipo de dato no válido")
+        return None
 
 def producto(num1, num2):
     try:
         resultado = float(num1) * float(num2)
+        return resultado
     except TypeError:
-        print("Error: Tipo de dato no válido.")
-        resultado = None
-    return resultado
+        print("Error: Tipo de dato no válido")
+        return None
 
 def division(num1, num2):
     try:
-        resultado = float(num1) / float(num2)
         if num2 == 0:
             raise ZeroDivisionError
-    except TypeError:
-        print("Error: Tipo de dato no válido.")
-        resultado = None
+        resultado = float(num1) / float(num2)
+        return resultado
     except ZeroDivisionError:
-        print("Error: No es posible dividir entre cero.")
-        resultado = None
-    return resultado
+        print("Error: No es posible dividir entre cero")
+        return None
+    except TypeError:
+        print("Error: Tipo de dato no válido")
+        return None
