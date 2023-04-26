be53bd422101c71c9992c9fa1087578cb3ec5340
maria2332 <maria.ballesteros2004@gmail.com>
2023-04-26 18:02:17 +0200

    5

--- pregunta5.py
+++ pregunta5.py
@@ -92,4 +92,85 @@ def eliminar(raiz, valor):
 # Función para encontrar el mínimo valor en el árbol
 def minimo_valor(raiz):
     actual = raiz
-    while actual.izquierdo is
+    while actual.izquierdo is not None:
+        actual = actual.izquierdo
+    return actual
+
+# Función para encontrar la altura de un árbol
+def altura(raiz):
+    if raiz is None:
+        return 0
+    altura_izq = altura(raiz.izquierdo)
+    altura_der = altura(raiz.derecho)
+    return 1 + max(altura_izq, altura_der)
+
+# Función para contar la cantidad de ocurrencias de un valor en el árbol
+def contar_ocurrencias(raiz, valor):
+    if raiz is None:
+        return 0
+    elif raiz.valor == valor:
+        return 1 + contar_ocurrencias(raiz.izquierdo, valor) + contar_ocurrencias(raiz.derecho, valor)
+    elif valor < raiz.valor:
+        return contar_ocurrencias(raiz.izquierdo, valor)
+    else:
+        return contar_ocurrencias(raiz.derecho, valor)
+
+# Función para contar la cantidad de números pares e impares en el árbol
+def contar_pares_impares(raiz, pares=0, impares=0):
+    if raiz is None:
+        return pares, impares
+    elif raiz.valor % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+    pares, impares = contar_pares_impares(raiz.izquierdo, pares, impares)
+    pares, impares = contar_pares_impares(raiz.derecho, pares, impares)
+    return pares, impares
+
+# Generar los 1000 números aleatorios y cargarlos en el árbol
+arbol = None
+for i in range(1000):
+    num = random.randint(1, 1000)
+    arbol = insertar(arbol, num)
+
+# Realizar los distintos tipos de recorrido del árbol
+print("Recorrido preorden:")
+preorden(arbol)
+print("\nRecorrido inorden:")
+inorden(arbol)
+print("\nRecorrido postorden:")
+postorden(arbol)
+print("\nRecorrido por nivel:")
+por_nivel(arbol)
+
+# Buscar si un valor está en el árbol
+buscar_valor = 500
+if buscar(arbol, buscar_valor):
+    print("\nEl valor", buscar_valor, "está en el árbol.")
+else:
+    print("\nEl valor", buscar_valor, "no está en el árbol.")
+
+# Eliminar tres valores del árbol
+eliminar_valor_1 = 250
+eliminar_valor_2 = 750
+eliminar_valor_3 = 1000
+arbol = eliminar(arbol, eliminar_valor_1)
+arbol = eliminar(arbol, eliminar_valor_2)
+arbol = eliminar(arbol, eliminar_valor_3)
+
+# Calcular la altura de los subárboles izquierdo y derecho
+altura_izq = altura(arbol.izquierdo)
+altura_der = altura(arbol.derecho)
+print("\nAltura del subárbol izquierdo:", altura_izq)
+print("Altura del subárbol derecho:", altura_der)
+
+# Contar la cantidad de ocurrencias de un valor en el árbol
+ocurrencias_valor = 500
+cantidad_ocurrencias = contar_ocurrencias(arbol, ocurrencias_valor)
+print("\nEl valor", ocurrencias_valor, "aparece", cantidad_ocurrencias, "veces en el árbol.")
+
+# Contar la cantidad de números pares e impares en el árbol
+pares, impares = contar_pares_impares(arbol)
+print("\nCantidad de números pares en el árbol:", pares)
+print("Cantidad de números impares en el árbol:", impares)
+
